94ba4ee7dfb4f2e2aba95dc7d3d410dbd5167090
Wojtalo94 <wojtek94@interia.eu>
2023-01-09 08:04:25 +0100

    Code repair and refactoring to reduce the number of PyCharm comments displayed. The test was able to run both in cmd using: py.test test_add_group.py, and with PyCharm using pytest.

--- test_add_group.py
+++ test_add_group.py
@@ -1,22 +1,15 @@
 # -*- coding: utf-8 -*-
-from webdriver_manager.firefox import GeckoDriverManager
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
-#        self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
     
     def test_add_group(self):
         wd = self.wd
@@ -32,27 +25,30 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("testGrup")
+        wd.find_element_by_name("group_name").send_keys("testGroup")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("testGrup")
+        wd.find_element_by_name("group_header").send_keys("testGroup")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("testGrup")
+        wd.find_element_by_name("group_footer").send_keys("testGroup")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
     
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to.alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-
     
     def tearDown(self):
         self.wd.quit()
