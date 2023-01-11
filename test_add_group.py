cb2224b34ac31352c0cd90ef754c30973d5acbd9
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-01-11 06:31:24 +0100

    Removal of unnecessary stepping stones that, such as clicking in a field to enter data, which is unnecessary. Tests pass without this.

--- test_add_group.py
+++ test_add_group.py
@@ -22,11 +22,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
@@ -37,14 +33,8 @@ class TestAddGroup(unittest.TestCase):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
