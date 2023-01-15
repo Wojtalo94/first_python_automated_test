b6819b226761e7054d7891d93b542f1968a3c2a7
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-01-15 12:24:20 +0100

    Extract SessionHelper helper class to manage sessions

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -12,14 +12,14 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Jan", middlename="Marek", lastname="Kowalski", nickname="Kowal", title="mgr", company="IT", address="Cracow 33-800", home_phone="111222333", mobile_phone="444555666", work_phone="777888999", fax="123456789", email="test1@gmail.com", email2="test2@gmail.com", email3="test3@gmail.com", homepage="www.test.com", bday="13", bmonth="June", byear="1992", aday="10", amonth="April", ayear="2013", address2="Test1", phone2="Test2", notes="Test3"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="", mobile_phone="", work_phone="", fax="", email="", email2="", email3="", homepage="", bday="-", bmonth="-", byear="", aday="-", amonth="-", ayear="", address2="", phone2="", notes=""))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
