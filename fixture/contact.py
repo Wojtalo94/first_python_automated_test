6d52f96e1c519ef381e186cceecb687c377711d4
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-02-04 15:03:59 +0100

    It was used to cut the strings by various means using the split or splitlines function. And also with the help of regular expressions we isolated the fragments we needed and performed the replacement (clear method) in order to bring the strings to a uniform appearance, omitting special symbols.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -88,7 +89,6 @@ class ContactHelper:
 
     def modify_contact_by_index(self, index, contact):
         wd = self.app.wd
-        self.open_main_page()
         self.select_contact_to_modify_by_index(index)
         # update contact
         self.fill_contact_details(contact)
@@ -99,6 +99,7 @@ class ContactHelper:
 
     def select_contact_to_modify_by_index(self, index):
         wd = self.app.wd
+        self.open_main_page()
         # Edit random contact
         wd.find_elements_by_css_selector('img[alt="Edit"]')[index].click()
 
@@ -142,5 +143,40 @@ class ContactHelper:
                 last = cells[1].text
                 first = cells[2].text
                 id = cells[0].find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(lastname=last, firstname=first, id=id))
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(lastname=last, firstname=first, id=id, home_phone=all_phones[0],
+                                                  mobile_phone=all_phones[1], work_phone=all_phones[2],
+                                                  phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def select_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_main_page()
+        # select created contact do view details
+        wd.find_elements_by_css_selector('img[alt="Details"]')[index].click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.select_contact_to_modify_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home_phone = wd.find_element_by_name("home").get_attribute("value")
+        mobile_phone = wd.find_element_by_name("mobile").get_attribute("value")
+        work_phone = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id, home_phone=home_phone,mobile_phone=mobile_phone,
+                       work_phone=work_phone, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.select_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_phone = re.search("H: (.*)", text).group(1)
+        mobile_phone = re.search("M: (.*)", text).group(1)
+        work_phone = re.search("W: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home_phone=home_phone, mobile_phone=mobile_phone, work_phone=work_phone, phone2=phone2)
+
+
+
