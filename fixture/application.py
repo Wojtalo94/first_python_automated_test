b27899889d859fa5bda13465eac4d6d851890899
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-02-08 15:37:42 +0100

    Parameterization of tests (Data Driven Testing) for creating groups and contacts. Passing parameters from the command line to tests at startup

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,7 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,12 +10,20 @@ from selenium import webdriver
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome(ChromeDriverManager().install())
+        elif browser == "ie":
+            self.wd = webdriver.Ie(executable_path=IEDriverManager().install())
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +34,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- tests/test_contact_details.py
+++ tests/test_contact_details.py
@@ -11,17 +11,13 @@ def test_contact_details_on_home_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
 
 
-def clear_phone_symbols(s):
+def clear(s):
     return re.sub("[() -]", "", s)
-
-
-def clear_email_symbols(s):
-    return re.sub("[() #$%^&+]", "", s)
 
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear_phone_symbols(x),
+                            map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.home_phone, contact.mobile_phone, contact.work_phone,
                                         contact.phone2]))))
@@ -29,6 +25,4 @@ def merge_phones_like_on_home_page(contact):
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear_email_symbols(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                            filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3])))
