5cf2ced947e4bd53e5070a508f291dd91f0331ef
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-02-11 14:54:38 +0100

    Generation of test data in JSONA URL format

--- test_data/add_group.py
+++ test_data/add_group.py
@@ -18,4 +18,3 @@ testdata = [Group(name="", header="", footer="")] + [
         Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 20))
         for i in range(5)
     ]
-
