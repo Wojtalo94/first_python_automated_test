e9eb3abba280c683a2c20cd152338f399499c66a
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-02-17 21:23:24 +0100

    Reworking tests: checking data, loading data from database.

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -2,11 +2,30 @@
 from model.group import Group
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+
+
+
+
+#--------------------------------------------------------------------------
+# Poniżej jest test, który korzysta z ładowania danych za pomocą UI (interfejs użytkownika)
+#--------------------------------------------------------------------------
+#from model.group import Group
+
+
+#def test_add_group(app, json_groups):
+#    group = json_groups
+#    old_groups = app.group.get_group_list()
+#    app.group.create(group)
+#    assert len(old_groups) + 1 == app.group.count()
+#    new_groups = app.group.get_group_list()
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
