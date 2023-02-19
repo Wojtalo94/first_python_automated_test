e90d9d53e0fbb292d1760bdb241f7409536394e0
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-02-19 12:39:31 +0100

    Checking the contact information on the home page, comparing with the information, loaded from the database.

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -19,6 +20,26 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, middlename, lastname, nickname, company, title, address, home, "
+                           "mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, "
+                           "ayear, address2, phone2, notes from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, middlename, lastname, nickname, company, title, address, home, mobile, work, fax, email,
+                 email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes) = row
+                list.append(Contact(id=str(id), firstname=firstname, middlename=middlename, lastname=lastname, nickname=
+                                    nickname, company=company, title=title, address=address, home_phone=home,
+                                    mobile_phone=mobile, work_phone=work, fax=fax, email=email, email2=email2, email3=
+                                    email3, homepage=homepage, bday=str(bday), bmonth=bmonth, byear=byear,
+                                    aday=str(aday), amonth=amonth, ayear=ayear, address2=address2, phone2=phone2,
+                                    notes=notes))
         finally:
             cursor.close()
         return list
