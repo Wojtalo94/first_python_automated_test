2b18bc69831fcc7953950028f89d6543e28426a7
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-02-26 16:51:22 +0100

    Fixing method get_contacts_not_in_group

--- fixture/orm.py
+++ fixture/orm.py
@@ -58,4 +58,4 @@ class ORMFixture:
     def get_contacts_not_in_group(self, group):
         orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
         return self.convert_contacts_to_model(
-            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.group))
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
